8c911a00658c387a037b2d3c88dcf610b27224f6
Arturo Vergara <hello@arturovm.me>
2016-10-31 10:32:34 -0600

    updated help text

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ import (
 
 var (
 	flagInterface    = flag.String("i", "eth0", `Network interface.`)
-	flagTarget       = flag.String("t", "", `Target host(s). Provide a single IP: "1.2.3.4", a list of IPs: "1.2.3.4, 5.6.7.8", a CIDR block "1.2.3.0/24" or an IP range: "1.2.3-7.4-12"`)
+	flagTarget       = flag.String("t", "", `Target host(s). Provide a single IP: "1.2.3.4", a CIDR block "1.2.3.0/24", an IP range: "1.2.3-7.4-12", an IP with a wildcard: "1.2.3.*", or a list with any combination: "1.2.3.4, 1.2.3.0/24, ..."`)
 	flagWaitInterval = flag.Float64("w", 5.0, `Wait <w> seconds between every broadcast, <w> must be a value greater than 0.1.`)
 	flagHelp         = flag.Bool("h", false, `Print usage instructions and exit.`)
 )
